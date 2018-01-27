c64a8cb42c6109f4eca533ba85d8ed5f10862b72
Ragkhuza <dargonhelsing@gmail.com>
2018-01-27 09:44:00 +0800

    fix app property not found in production

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\updateImage;
 use App\Image;
-use App\property;
+use App\Property;
 use Illuminate\Http\Request;
 
 class PropertyController extends Controller
