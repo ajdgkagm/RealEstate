8fd51dd0637b237e7f45257c143b837eca151889
Ragkhuza <dargonhelsing@gmail.com>
2018-01-28 17:23:45 +0800

    imported mapper

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -9,7 +9,7 @@ use App\Property;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Mapper;
+use Cornford\Googlmapper\Facades\MapperFacade as Mapper;;
 
 class PropertyController extends Controller
 {
